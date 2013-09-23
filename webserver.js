47c7d1a7c40192b6aa1510a17225f956eb74e789
Adam Ulvi <adam@delaris.com>
2013-09-22 22:30:00 -0700

    Initial check-in.

---
+++ webserver.js
@@ -0,0 +1,45 @@
+// webserver.js
+// Simple restify server
+
+var restify = require('restify')
+	, sio = require('socket.io')
+	, fs = require('fs')
+;
+
+module.exports = function() { 
+
+	var server = restify.createServer();
+	var io = sio.listen(server);
+
+	server.get(/\/?.*/, restify.serveStatic({
+		directory:'./public'
+	}));
+
+	/*
+	server.get('/', function indexHTML(req, res, next) {
+		fs.readFile(__dirname + '/index.html', function (err, data) {
+			if (err) {
+				next(err);
+				return;
+			}
+
+			res.setHeader('Content-Type', 'text/html');
+			res.writeHead(200);
+			res.end(data);
+			next();
+		});
+	});
+	*/
+
+	io.sockets.on('connection', function (socket) {
+		socket.emit('news', { hello: 'world' });
+		socket.on('my other event', function (data) {
+				console.log(data);
+		});
+	});
+
+	server.listen(8080, function () {
+		console.log('Swarm webserver listening at %s', server.url);
+	});
+
+};
