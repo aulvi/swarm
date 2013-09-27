de422398dc9d2ca70f4cc2a9aaa5ec84879232d5
Adam Ulvi <adam@delaris.com>
2013-09-26 20:16:05 -0700

    Added socket.io support

--- public/js/fly.js
+++ public/js/fly.js
@@ -1,3 +1,7 @@
 ;(function(){
-	console.log("Flying!");
+	var socket = io.connect("http://localhost:8080");
+	socket.on("news", function (data) {
+		console.log(data);
+		socket.emit("my other event", { my: "data" });
+	});
 }());
