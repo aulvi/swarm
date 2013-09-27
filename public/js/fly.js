0fc3554f9144adc1f828b0b8778f546d5f6e8a94
Adam Ulvi <adam@delaris.com>
2013-09-26 21:19:31 -0700

    Added super basic accelerometer support.

--- public/js/fly.js
+++ public/js/fly.js
@@ -1,7 +1,28 @@
-;(function(){
+;(function($){
 	var socket = io.connect("http://localhost:8080");
 	socket.on("news", function (data) {
 		console.log(data);
 		socket.emit("my other event", { my: "data" });
 	});
-}());
+
+	// accel
+	var sensitivity = 1;
+
+
+	window.ondevicemotion = function(event) {
+
+		var movement = 10;
+		var accX = Math.round(event.accelerationIncludingGravity.x*10) / 10;  
+		var accY = Math.round(event.accelerationIncludingGravity.y*10) / 10;  
+		var accZ = Math.round(event.accelerationIncludingGravity.z*10) / 10;  
+				
+		xA = -(accX / 10) * movement;
+		yA = -(accY / 10) * movement;
+		zA = -(accZ / 10) * movement;
+
+		$("#x").html(xA);
+		$("#y").html(yA);
+		$("#z").html(zA);
+	};
+
+}(jQuery));
