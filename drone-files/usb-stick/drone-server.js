bd30d1ba013c589ee262b5407a24f40d431e4f76
Adam Ulvi <adam@delaris.com>
2013-10-22 00:01:57 -0700

    drone-files/

---
+++ drone-files/usb-stick/drone-server.js
@@ -0,0 +1,28 @@
+var 
+	serialport = require( 'node-serialport' ),
+	io = require( 'socket.io' ).connect( 1337 )
+;
+ 
+var sp = new serialport.SerialPort("/dev/ttyO3", { 
+  parser: serialport.parsers.raw,
+  baud: 9600
+});
+ 
+io.sockets.on('connection', function (socket) {
+	console.log("Server connected!");
+
+	socket.on('data', function (data) {
+		console.log("Data received! " + JSON.stringify(data));
+	});
+
+	sp.on('data', function(chunk) {
+		var data = chunk.toString();
+		console.log("Sending: " + data);
+		socket.emit('data', data);
+
+//		console.log(chunk.toString('hex'), ), chunk);
+	});
+
+});
+
+
