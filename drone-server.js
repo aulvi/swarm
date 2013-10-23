d5122bff47a717974e0deebcb601b6c8dafd2d14
Adam Ulvi <adam@delaris.com>
2013-10-23 14:24:50 -0700

    Wrapped the ar-drone library, I think we've got most of the messaging framework in place meow.

--- drone-server.js
+++ drone-server.js
@@ -1,5 +1,9 @@
 var server = require( "./lib/drone-server" );
+var arDroner = require ( "./lib/ardroner" );
+var mockDrone = require( "./test/mocks/mock.ar-drone" );
+var drone = new arDroner("127.0.0.1", mockDrone);
 
 server.on("cmd", function(data){
-	console.log("Drone will: " + data[0] + "(" + data[1] + ")");
+	console.log( "Drone will: " + data[0] + "(" + data[1] + ")" );
+	drone.send( data );
 });
