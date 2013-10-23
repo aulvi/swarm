c0a700cc411c9d811fc62baae853bada805d5456
Adam Ulvi <adam@delaris.com>
2013-10-23 13:50:47 -0700

    Plumbing

--- drone-server.js
+++ drone-server.js
@@ -1,5 +1,5 @@
 var server = require( "./lib/drone-server" );
 
 server.on("cmd", function(data){
-	console.log("CMD received: " + data);
+	console.log("Drone will: " + data[0] + "(" + data[1] + ")");
 });
